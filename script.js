7b8b9b7c59c25878a85fb8f1742a72ebada253a8
Mohamed <justmoe23@gmail.com>
2022-09-19 18:27:39 -0400

    add live clock feature and adjust media queries for iphone

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ musicButton.onclick = function () {
     musicButton.innerText = "Play Background Music";
   }
 };
+
+function showDate() {
+  const weekday = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  const date = new Date();
+
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let day = weekday[date.getDay()];
+
+  if (hour > 12) {
+    hour = hour - 12;
+  }
+
+  if (hour === 0) {
+    hour = 12;
+  }
+
+  if (minute >= 0 && minute < 10) {
+    minute = "0" + minute;
+  }
+
+  document.getElementById("time").innerText = day + " " + hour + ":" + minute;
+}
+
+setInterval(showDate, 100);
