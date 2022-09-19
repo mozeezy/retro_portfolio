6607d09b0977a556705b265caeb93be01b938543
Mohamed <justmoe23@gmail.com>
2022-09-19 16:58:45 -0400

    update stop button functionality

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let mySong = document.getElementById("mySong");
-        let musicButton = document.getElementById("musicButton");
+let musicButton = document.getElementById("musicButton");
 
-        musicButton.onclick = function() {
-          if(mySong.paused) {
-            mySong.play();
-            musicButton.className = "nes-btn is-error"
-            musicButton.innerText = "Pause"
-          } else {
-            mySong.pause();
-            musicButton.className = "nes-btn is-success"
-            musicButton.innerText = "Play Background Music"
-          }
-        }
+musicButton.onclick = function () {
+  if (mySong.paused) {
+    mySong.play();
+    musicButton.className = "nes-btn is-error";
+    musicButton.innerText = "Stop";
+  } else {
+    mySong.pause();
+    mySong.currentTime = 0;
+    musicButton.className = "nes-btn is-success";
+    musicButton.innerText = "Play Background Music";
+  }
+};
